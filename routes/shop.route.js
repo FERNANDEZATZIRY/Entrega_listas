6c8c0fd3841477de2cfd221170e4f7747277d68e
FERNANDEZATZIRY <124528995+FERNANDEZATZIRY@users.noreply.github.com>
2023-06-04 13:04:04 -0600

    Add files via upload

---
+++ routes/shop.route.js
@@ -0,0 +1,32 @@
+// Importando el enrutador de express
+import { Router } from 'express';
+// Importando el arreglo de productos
+import { products } from './admin.route.js';
+
+// Creando una instancia del enrutador de express
+const router = Router();
+
+// GET /
+router.get('/', (req, res) => {
+  // Mostrando productos en memoria
+  console.log(products);
+  console.log("📢 Sirviendo la ruta '/'");
+  res.render('shop', {
+    shop: 'active',
+    docTitle: "Shop",
+    viewStyle: '/css/product.css',
+    products
+  });
+});
+
+// GET /about
+router.get('/about', (req, res) => {
+  console.log("📢 Sirviendo la ruta '/about'");
+  // Se contesta al server
+  res.send(`
+    <h1>🪄 About...</h1>
+    <p>App for Fullstack Web Dev Course I!</p>
+  `);
+});
+
+export default router;
